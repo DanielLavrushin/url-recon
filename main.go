3d1435b97d4978206a9795f1ee2838a835659c5d
Daniel Lavrushin <lavrushin@gmail.com>
2026-04-04 20:06:14 +0200

    feat: disable buffering for SSE responses in handleJobStream

--- main.go
+++ main.go
@@ -294,6 +294,7 @@ func handleJobStream(w http.ResponseWriter, r *http.Request, jobID string) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Accel-Buffering", "no")
 
 	flusher, ok := w.(http.Flusher)
 	if !ok {
